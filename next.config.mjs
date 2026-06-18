d5c6a77f5720e906ec17f6e0715ce5b617730ada
devObuzt <63292157+devObuzt@users.noreply.github.com>
2026-06-18 14:50:30 +0100

    fix: allow large media request bodies

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    middlewareClientMaxBodySize: "64mb"
+  },
   serverExternalPackages: []
 };
 
 export default nextConfig;
-
